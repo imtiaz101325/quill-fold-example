fdf503fc0336714851d0ed822e124e8712fd54ad
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-07-27 04:19:22 +0600

    Fixed new lines with no event listner issue

--- src/CustomListItem.ts
+++ src/CustomListItem.ts
@@ -4,56 +4,60 @@ import "./custom_list_item.css";
 
 const ListItem = Quill.import("formats/list/item");
 
+const eventHandler = (event: MouseEvent) => {
+  const element = <HTMLDListElement>event.target;
+
+  if (element) {
+    let indentLevel = 0;
+    element.classList.forEach((className) => {
+      const parts = className.split("-");
+      if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
+        indentLevel = parseInt(parts[2], 10);
+      }
+    });
+
+    let sibling = element.nextElementSibling;
+    while (sibling) {
+      if (sibling.classList.contains("custom_list_item")) {
+        let siblingIndentLevel = 0;
+        sibling.classList.forEach((className) => {
+          const parts = className.split("-");
+          if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
+            siblingIndentLevel = parseInt(parts[2], 10);
+          }
+        });
+
+        if (indentLevel === siblingIndentLevel) {
+          break;
+        }
+
+        if (sibling.classList.contains("hide")) {
+          sibling.classList.remove("hide");
+        } else {
+          sibling.classList.add("hide");
+        }
+
+        sibling = sibling.nextElementSibling;
+      } else {
+        break;
+      }
+    }
+  }
+};
 export default class Custom extends ListItem {
   static create(value: string): Node {
     const node = super.create();
     node.classList.add("custom_list_item");
 
-    node.addEventListener("click", (event: MouseEvent) => {
-      const element = <HTMLDListElement>event.target;
-
-      if (element) {
-        let indentLevel = 0;
-        element.classList.forEach((className) => {
-          const parts = className.split("-");
-          if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
-            indentLevel = parseInt(parts[2], 10);
-          }
-        });
-
-        let sibling = element.nextElementSibling;
-        while (sibling) {
-          if (sibling.classList.contains("custom_list_item")) {
-            let siblingIndentLevel = 0;
-            sibling.classList.forEach((className) => {
-              const parts = className.split("-");
-              if (
-                parts.length > 2 &&
-                parts[0] === "ql" &&
-                parts[1] === "indent"
-              ) {
-                siblingIndentLevel = parseInt(parts[2], 10);
-              }
-            });
-
-            if (indentLevel === siblingIndentLevel) {
-              break;
-            }
-
-            if (sibling.classList.contains("hide")) {
-              sibling.classList.remove("hide");
-            } else {
-              sibling.classList.add("hide");
-            }
-
-            sibling = sibling.nextElementSibling;
-          } else {
-            break;
-          }
-        }
-      }
-    });
+    node.addEventListener("click", eventHandler);
 
     return node;
+  }
+
+  clone() {
+    const clone = super.clone();
+    clone.domNode.addEventListener("click", eventHandler)
+
+    return clone;
   }
 }
