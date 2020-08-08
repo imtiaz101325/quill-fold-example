2db02469f8a4d8ec5d457fff02eb2805407dccee
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-08-09 00:21:39 +0600

    fixed nested list indentation

--- src/CustomListItem.ts
+++ src/CustomListItem.ts
@@ -4,6 +4,18 @@ import "./custom_list_item.css";
 
 const ListItem = Quill.import("formats/list/item");
 
+function getIndentLevel(element: Element) {
+  let indentLevel = 0;
+  element.classList.forEach((className) => {
+    const parts = className.split("-");
+    if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
+      indentLevel = parseInt(parts[2], 10);
+    }
+  });
+
+  return indentLevel;
+}
+
 const eventHandler = (event: MouseEvent) => {
   const element = <HTMLDListElement>event.target;
 
@@ -16,24 +28,12 @@ const eventHandler = (event: MouseEvent) => {
   }
 
   if (element) {
-    let indentLevel = 0;
-    element.classList.forEach((className) => {
-      const parts = className.split("-");
-      if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
-        indentLevel = parseInt(parts[2], 10);
-      }
-    });
+    const indentLevel = getIndentLevel(element);
 
     let sibling = element.nextElementSibling;
     while (sibling) {
       if (sibling.classList.contains("custom_list_item")) {
-        let siblingIndentLevel = 0;
-        sibling.classList.forEach((className) => {
-          const parts = className.split("-");
-          if (parts.length > 2 && parts[0] === "ql" && parts[1] === "indent") {
-            siblingIndentLevel = parseInt(parts[2], 10);
-          }
-        });
+        const siblingIndentLevel = getIndentLevel(sibling);
 
         if (siblingIndentLevel <= indentLevel) {
           break;
@@ -41,11 +41,27 @@ const eventHandler = (event: MouseEvent) => {
 
         if (mode === "SHOW") {
           sibling.classList.remove("hide");
+          if (sibling.classList.contains("toggle")) {
+            let nextSibling = sibling.nextElementSibling;
+            while (nextSibling) {
+              const nextSiblingIndentLevel = getIndentLevel(nextSibling);
+
+              if (nextSiblingIndentLevel <= siblingIndentLevel) {
+                break;
+              }
+
+              nextSibling = nextSibling.nextElementSibling;
+            }
+
+            sibling = nextSibling;
+          }
         } else {
           sibling.classList.add("hide");
         }
 
-        sibling = sibling.nextElementSibling;
+        if (sibling) {
+          sibling = sibling.nextElementSibling;
+        }
       } else {
         break;
       }
@@ -65,7 +81,7 @@ export default class Custom extends ListItem {
 
   clone() {
     const clone = super.clone();
-    clone.domNode.addEventListener("click", eventHandler)
+    clone.domNode.addEventListener("click", eventHandler);
 
     return clone;
   }
